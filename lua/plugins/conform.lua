0c3aeef4159359e4858444a324c787f0c0b0dfc1
Ryan G Mazambani <220849443+ryanmazg@users.noreply.github.com>
2025-08-06 16:22:31 +0200

    configure conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,23 @@
 return {
   'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  opts = {
+    formatters_by_ft = {
+      python = { "ruff", "black" },
+    },
+    format_on_save = {
+      timeout_ms = 1000,
+      lsp_fallback = true,
+    },
+  },
+  keys = {
+    {
+      '<leader>fm',
+      function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end,
+      mode = { 'n', 'v' },
+    },
+  },
 }
