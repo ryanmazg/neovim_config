320fd8ef69ab2defa08dbbe345e377fc12bd8aea
Ryan G Mazambani <220849443+ryanmazg@users.noreply.github.com>
2025-08-06 11:48:58 +0200

    configure basic options

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+vim.o.breakindent = true
+vim.o.colorcolumn = "80"
+vim.o.confirm = true
+vim.o.cursorline = true
+vim.o.expandtab = true
+vim.o.ignorecase = true
+vim.o.mouse = ""
+vim.o.number = true
+vim.o.relativenumber =true
+vim.o.scrolloff = 8
+vim.o.shiftwidth = 4
+vim.o.smartcase  = true
+vim.o.softtabstop = 4
+vim.o.splitright = true
+vim.o.tabstop = 4
+vim.o.undofile = true
+vim.o.wrap = false
