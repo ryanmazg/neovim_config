d0f3e58fd187f1b28fcdaf22d79cf5fe2276bb70
Ryan G Mazambani <220849443+ryanmazg@users.noreply.github.com>
2025-08-06 16:27:40 +0200

    install and configure autopairs

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,7 +11,12 @@ return {
   },
 
   config = function()
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require'cmp'
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
 
     cmp.setup({
       snippet = {
