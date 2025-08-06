cdeb582815c0d4a7bdebbe2575bfd8626b1d0947
Ryan G Mazambani <220849443+ryanmazg@users.noreply.github.com>
2025-08-06 15:30:46 +0200

    configure telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,4 +5,22 @@ return {
     "nvim-telescope/telescope-fzf-native.nvim",
     "nvim-tree/nvim-web-devicons",
   },
+
+  config = function()
+    require('telescope').setup{}
+    local builtin = require('telescope.builtin')
+
+    vim.keymap.set('n', '<leader>fF', builtin.find_files)
+    vim.keymap.set('n', '<leader>ff', builtin.git_files)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string)
+    vim.keymap.set('n', '<leader>b', builtin.buffers)
+    vim.keymap.set('n', '<leader>q', builtin.quickfix)
+    vim.keymap.set('n', '<leader>qh', builtin.quickfixhistory)
+    vim.keymap.set('n', '<leader>l', builtin.loclist)
+    vim.keymap.set('n', '<leader>s', builtin.current_buffer_fuzzy_find)
+    vim.keymap.set('n', '<leader>r', builtin.lsp_references)
+    vim.keymap.set('n', '<leader>d', builtin.diagnostics)
+    vim.keymap.set('n', '<leader>i', builtin.lsp_implementations)
+  end,
 }
