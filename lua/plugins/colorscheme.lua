898f36db02d289a53fe84230a81ac3eea8bc2acc
Ryan G Mazambani <220849443+ryanmazg@users.noreply.github.com>
2025-08-09 04:45:02 +0200

    modify cmp keymaps

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,13 @@ return {
 		config = function()
 			require("onedark").setup({
 				style = "darker",
+				code_style = {
+					comments = "italic",
+					keywords = "bold",
+					functions = "bold",
+					strings = "none",
+					variables = "none",
+				},
 			})
 
 			require("onedark").load()
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -28,9 +28,8 @@ return {
 			},
 
 			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
