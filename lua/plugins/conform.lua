2cd2c86205f4c296e15e223844bfedd28761d72d
Ryan G Mazambani <220849443+ryanmazg@users.noreply.github.com>
2025-08-11 16:02:03 +0200

    Configure formatters to use 4 space indentation

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,36 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	opts = {
-		formatters_by_ft = {
-			python = { "black" },
-			lua = { "stylua" },
-			html = { "prettier" },
-			css = { "prettier" },
-			javascript = { "prettier" },
-		},
-		format_on_save = {
-			timeout_ms = 1000,
-			lsp_fallback = true,
-		},
-	},
-	keys = {
-		{
-			"<leader>fm",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = { "n", "v" },
-		},
-	},
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+        formatters_by_ft = {
+            python = { "black" },
+            lua = { "stylua" },
+            html = { "prettier" },
+            css = { "prettier" },
+            javascript = { "prettier" },
+        },
+        formatters = {
+            black = {},
+            stylua = {
+                prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+            },
+            prettier = {
+                prepend_args = { "--tab-width", "4", "--use-tabs", "false" },
+            },
+        },
+        format_on_save = {
+            timeout_ms = 1000,
+            lsp_fallback = true,
+        },
+    },
+    keys = {
+        {
+            "<leader>fm",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = { "n", "v" },
+        },
+    },
 }
